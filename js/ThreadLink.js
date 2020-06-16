dd70574e77c4a82a7cdc8e723f3d936d8e0e9431
Jonakss <jona.correapc@gmail.com>
2020-06-16 00:47:19 -0300

    Added threads now has his button

--- js/ThreadLink.js
+++ js/ThreadLink.js
@@ -1,31 +1,48 @@
 console.log("Google Chrome extension GCThreadLink loaded");
 
-let threads = document.getElementsByClassName("cZICLc");
+let addButton = function(){
+  let threads = document.getElementsByClassName("cZICLc");
 
-for (thr of threads){
-  let topicId = thr.getAttribute("data-topic-id");
-  let link = window.location.href + "/" + topicId;
+  for (thr of threads){
+    if(thr.getElementsByClassName("link").length === 0){
+      let topicId = thr.getAttribute("data-topic-id");
+      let link = window.location.href + "/" + topicId;
 
-  let buttonTarget = thr.getElementsByClassName("yg4pvb")[0];
+      let buttonTarget = thr.getElementsByClassName("yg4pvb")[0];
 
-  let linkButton = document.createElement("div");
-  linkButton.classList.add("oQXyRb");
+      let linkButton = document.createElement("div");
+      linkButton.classList.add("link");
+      linkButton.classList.add("oQXyRb");
 
-  let linkSpan = document.createElement("span");
-  linkSpan.setAttribute("style", "display : none;");
-  linkSpan.innerHTML = link;
+      let linkSpan = document.createElement("span");
+      linkSpan.setAttribute("style", "display : none;");
+      linkSpan.innerHTML = link;
 
 
-  let spanLinkButton = document.createElement("span");
-  spanLinkButton.classList.add("zxHSSc");
-  spanLinkButton.setAttribute("style", "display : inline;");
-  spanLinkButton.innerHTML = "Copiar";
+      let spanLinkButton = document.createElement("span");
+      spanLinkButton.classList.add("zxHSSc");
+      spanLinkButton.setAttribute("style", "display : inline;");
+      spanLinkButton.innerHTML = "Copiar";
 
-  buttonTarget.appendChild(linkButton);
-  linkButton.appendChild(spanLinkButton);
-  linkButton.appendChild(linkSpan);
+      buttonTarget.appendChild(linkButton);
+      linkButton.appendChild(spanLinkButton);
+      linkButton.appendChild(linkSpan);
 
-  linkButton.onclick = function(){
-    navigator.clipboard.writeText(this.children[1].innerHTML);
+      linkButton.onclick = function(){
+        navigator.clipboard.writeText(this.children[1].innerHTML);
+      };
+    };
   };
-}
+};
+
+let threadListWrapper = document.getElementsByClassName("SvOPqd")[0];
+
+const observer = new MutationObserver(() => {
+  addButton();
+});
+
+const observerOptions = { childList: true };
+
+observer.observe(threadListWrapper, observerOptions);
+
+addButton();
